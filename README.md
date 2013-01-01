4264f63b701158ed8b6f15d00796c9d093541813
Mattijs Hoitink <mattijs@monkeyandmachine.com>
2013-01-01 14:39:24 +0100

    First version (actually the second)

---
+++ README.md
@@ -0,0 +1,264 @@
+# Rsync
+
+## Installation
+
+Installation goes through NPM:
+
+```
+$ npm install rsync
+```
+
+## License
+
+This module is licensed under the MIT License. See the `LICENSE` file for more details.
+
+## Simple usage
+
+```javascript
+var Rsync = require('rsync');
+
+// Build the command
+var rsync = new Rsync()
+  .shell('ssh')
+  .flags('az')
+  .source('/path/to/source')
+  .destination('server:/path/to/destination');
+
+// Execute the command
+rsync.execute(function(error, stdout, stderr) {
+    // we're done
+});
+```
+
+For more examples see the `examples` directory.
+
+# API
+
+  * [constructor](#constructor)
+  * [instance methods](#instance-methods)
+  * [accessor methods](#accessor-methods)
+  * [static methods](#static-methods)
+
+## constructor
+
+Construct a new Rsync command instance. The constructor takes no arguments.
+
+```javascript
+var rsync = new Rsync();
+```
+
+## instance methods
+
+### set(option, value)
+
+Set an option. This can be any option from the rsync manual. The value is optional and only applies to options that take a value. This is not checked however. Supplying a value for an option that does not take a value will append the value regardless. This may cause errors when the command is executed.
+
+```javascript
+rsync.set('a')
+  .set('progress')
+  .set('list-only')
+  .set('exclude-from', '/path/to/exclude-file');
+```
+
+Options must be unique and setting the same option twice will override any existing value. For options that can be set multiple times special methods exist (see accessor methods). Any leading dashes (-) are stripped when setting the option.
+
+The `set` method is chainable.
+
+### unset(option)
+
+Unset an option. Any leading dashes (-) are stripped when unsetting an option.
+
+```javascript
+rsync.unset('progress')
+  .unset('quiet');
+```
+
+The `unset` method is chainable.
+
+### flags(flags, set)
+
+Set one or more flags. Flags are single letter options without a value, for example _compress_ (`-z`) or _archive_ (`-a`).
+
+The `flags` method is a polymorphic function: it can take different arguments to set flags.
+Flags can be presented as a single string with multiple flags, multiple strings as arguments, an array containing strings or an object with the flags as keys.
+
+Whether the presented flags need to be set or unset is determined based on the last argument, if this is a Boolean. When presenting the flags as an Object the value for each key (flag) determines if the flag is set or unset. This version can be used to mix setting and unsetting of flags in one statement.
+
+```javascript
+// As String
+rsync.flags('avz');        // set
+rsync.flags('avz', false); // unset
+
+// As String arguments
+rsync.flags('a', 'v', 'z');        // set
+rsync.flags('a', 'v', 'z', false); // unset
+
+// As Array
+rsync.flags(['a', 'v', 'z']);   // set
+rsync.flags(['a', 'z'], false); // unset
+
+// As Object
+rsync.flags({
+  'a': true, // set
+  'z': true, // set
+  'v': false // unset
+});
+```
+
+The `flags` method is chainable.
+
+### isSet(option)
+
+Check if an option is set.
+
+This method does not check alternate versions for an option. When an option is set as the short version this method will still return `false` when checking for the long version, event though they are the same option.
+
+```javascript
+rsync.set('quiet');
+rsync.isSet('quiet'); // is TRUE
+rsync.isSet('q');     // is FALSE
+```
+
+### option(option)
+
+Get the value for an option by name. If a valueless option is requested null will be returned.
+
+```javascript
+rsync.option('rsh');      // returns String value
+rsync.option('progress'); // returns NULL
+```
+
+### args()
+
+Get the arguments list for the command that is going to be executed. Returns an Array with the complete options that will be passed to the command.
+
+### command()
+
+Get the complete command that is going to be executed.
+
+```javascript
+var rsync = new Rsync()
+  .shell('ssh')
+  .flags('az')
+  .source('/p/t/source')
+  .destination('server:/p/t/dest');
+
+var c = rsync.command();
+// c is "rsync -az --rsh="ssh" /p/t/source server:/p/t/dest
+```
+### execute(callback)
+
+Executed the command.
+
+## option shorthands
+
+The following option shorthand methods are available:
+
+  - **shell(value)**: `--rsh=SHELL`
+  - **progress()**: `--progress`
+  - **archive()**: `-a`
+  - **compress()**: `-z`
+  - **recursive()**: `-r`
+  - **update()**: `-u`
+  - **quiet()**: `-q`
+  - **dirs()**: `-d`
+  - **links()**: `-l`
+  - **dry()**: `-n`
+
+All shorthand methods are chainable as long as options that require a value are provided with one.
+
+## accessor methods
+
+These methods can be used to get or set values in a chainable way. When the methods are called without arguments the current value is returned. When the methods are called with a value this will override the current value and the Rsync instance is returned to provide the chainability.
+
+### executable(executable)
+
+Get or set the executable to use as the rsync command.
+
+### destination(destination)
+
+Get or set the destination for the rsync command.
+
+### source(source)
+
+Get or set the source or sources for the rsync command. When this method is called multiple times with a value it is appended to the list of sources. It is also possible to present the list of source as an array where each value will be appended to the list of sources
+
+```javascript
+// chained
+rsync.source('/a/path')
+  .source('/b/path');
+
+// as Array
+rsync.source(['/a/path', '/b/path']);
+```
+
+In both cases the list of sources will contain two paths.
+
+### exclude(pattern)
+
+Exclude a pattern from transfer. When this method is called multiple times with a value it is appended to the list of excluded patterns. It is also possible to present the list of excluded patterns as an array where each pattern will be appended to the list.
+
+```javascript
+// chained
+rsync.exclude('.git')
+  .exclude('.DS_Store');
+
+// as Array
+rsync.exclude(['.git', '.DS_Store']);
+```
+
+### include(pattern)
+
+Include a pattern for transfer. When this method is called multiple times with a value it is appended to the list of included patterns. It is also possible to present the list of included patterns as an array where each pattern will be appended to the list.
+
+```javascript
+// chained
+rsync.include('/a/file')
+  .include('/b/file');
+
+// as Array
+rsync.include(['/a/file', '/b/file']);
+```
+
+### debug(flag)
+
+Get or set the debug flag. This is only used internally and must be a Boolean to set or unset.
+
+## static methods
+
+### build
+
+For convenience there is the `build` function on the Rsync contructor. This function can be
+used to create a new Rsync command instance from an options object.
+
+For each key in the options object the corresponding method on the Rsync instance will be
+called. When a function for the key does not exist it is ignored. An existing Rsync instance
+can optionally be provided.
+
+```javascript
+var rsync = Rsync.build({
+  source:      '/path/to/source',
+  destination: 'server:/path/to/destination',
+  exclude:     ['.git'],
+  flags:       'avz',
+  shell:       'ssh'
+});
+
+rsync.execute(function(error, stdout, stderr) {
+  // we're done
+});
+```
+
+# Development
+
+If there is something missing (which there probably is) just fork, patch and send a pull request.
+
+For adding a new shorthand method there are a few simple steps to take:
+- Fork
+- Add the option through the `exposeShortOption` or `exposeLongOption` functions. For examples see the source file.
+- Update this README file to list the new shorthand method
+- Make a pull request
+
+When adding a shorthand make sure it does not already exist, it is a sane name and a shorthand is necessary.
+
+If there is something broken (which there probably is), the same applies: fork, patch, pull request. Opening an issue is also possible.
---
+++ rsync.js
@@ -0,0 +1,449 @@
+var exec = require('child_process').exec;
+
+/**
+ * Rsync wrapper.
+ *
+ * @author      Mattijs Hoitink <mattijs@monkeyandmachine.com>
+ * @copyright   Copyright (c) 2013, Mattijs Hoitink <mattijs@monkeyandmachine.com>
+ * @license     The MIT License
+ */
+function Rsync(options) {
+    // executable
+    this._executable = 'rsync';
+
+    // source(s) and destination
+    this._sources     = [];
+    this._destination = '';
+
+    // included/excluded files
+    this._includes = [];
+    this._excludes = [];
+
+    // options
+    this._options = {};
+
+    // internal debugging flag
+    this._debug = false;
+}
+
+/**
+ * Build a new Rsync command from an options Object.
+ * @param {Object} options
+ * @return {Rsync}
+ */
+Rsync.build = function(options) {
+    var command = new Rsync();
+
+    // Process all options
+    for (var key in options) {
+        if (hasOP(options, key)) {
+            var value = options[key];
+
+            // Only allow calling methods on the Rsync command
+            if (typeof(command[key]) === 'function') {
+                command[key](value);
+            }
+        }
+    }
+
+    return command;
+};
+
+/**
+ * Set an option.
+ * @param {String} option
+ * @param mixed value
+ * @return Rsync
+ */
+Rsync.prototype.set = function(option, value) {
+    option = stripLeadingDashes(option);
+    if (option && option.length > 0) {
+        this._options[option] = value || null;
+    }
+    return this;
+};
+
+/**
+ * Unset an option.
+ * @param {String} option
+ * @return Rsync
+ */
+Rsync.prototype.unset = function(option) {
+    option = stripLeadingDashes(option);
+
+    if (option && Object.keys(this._options).indexOf(option) >= 0) {
+        delete this._options[option];
+    }
+    return this;
+};
+
+/**
+ * Set or unset one or more flags. A flag is a single letter option without a value.
+ *
+ * Flags can be presented as a single String, an Array containing Strings or an Object
+ * with the flags as keys.
+ *
+ * When flags are presented as a String or Array the set or unset method will be determined
+ * by the second parameter.
+ * When the flags are presented as an Object the set or unset method will be determined by
+ * the value corresponding to each flag key.
+ *
+ * @param {String|Array|Object} flags
+ * @param {Boolean} set
+ * @return Rsync
+ */
+Rsync.prototype.flags = function(flags, set) {
+    // Do some argument handling
+    if (!arguments.length) {
+        return this;
+    }
+    else if (arguments.length === 1) {
+        set = true;
+    }
+    else {
+        // There are more than 1 arguments, assume flags are presented as strings
+        flags = Array.prototype.slice.call(arguments);
+
+        // Check if the last argument is a boolean
+        if (typeof(flags[flags.length - 1]) === 'boolean') {
+            set = flags.pop();
+        }
+        else {
+            set = true;
+        }
+    }
+
+    // Split multiple flags
+    if (typeof(flags) === 'string') {
+        flags = stripLeadingDashes(flags).split('');
+    }
+
+    // Turn array into an object
+    if (isArray(flags)) {
+        var obj = {};
+        flags.forEach(function(f) {
+            obj[f] = set;
+        });
+        flags = obj;
+    }
+
+    // set/unset each flag
+    for (var key in flags) {
+        if (hasOP(flags, key)) {
+            var method = (flags[key]) ? 'set' : 'unset';
+            this[method](stripLeadingDashes(key));
+        }
+    }
+
+    return this;
+};
+
+/**
+ * Check if an option is set.
+ * @param {String} option
+ * @return {Boolean}
+ */
+Rsync.prototype.isSet = function(option) {
+    option = stripLeadingDashes(option);
+    return Object.keys(this._options).indexOf(option) >= 0;
+};
+
+/**
+ * Get an option by name.
+ * @param {String} name
+ * @return mixed
+ */
+Rsync.prototype.option = function(name) {
+    return this._options[name];
+};
+
+/**
+ * Get the command that is going to be executed.
+ * @return {String}
+ */
+Rsync.prototype.command = function() {
+    return this.executable() + ' ' + this.args().join(' ');
+};
+
+/**
+ * String representation of the Rsync command. This is the command that is
+ * going to be executed when calling Rsync::execute.
+ * @return {String}
+ */
+Rsync.prototype.toString = Rsync.prototype.command;
+
+/**
+ * Get the arguments for the rsync command.
+ * @return {Array}
+ */
+Rsync.prototype.args = function() {
+    // Gathered arguments
+    var args = [];
+
+    // Add options. Short options (one letter) without values are gathered together.
+    // Long options have a value but can also be a single letter.
+    var short = [];
+    var long  = [];
+
+    // Split long and short options
+    for (var key in this._options) {
+        if (hasOP(this._options, key)) {
+            var value = this._options[key];
+            var noval = (value === null || value === undefined);
+
+            // Check for short option (single letter without value)
+            if (key.length === 1 && noval) {
+                short.push(key);
+            }
+            else {
+                long.push(buildOption(key, value));
+            }
+
+        }
+    }
+
+    // Add short options if any were found
+    if (short.length > 0) args.push('-' + short.join(''));
+
+    // Add long options if any were found
+    if (long.length > 0)  args.push(long.join(' '));
+
+    // Add excludes
+    if (this._excludes.length > 0) {
+        args.push(this._excludes.map(function(pattern) {
+            return buildOption('exclude', pattern);
+        }).join(' '));
+    }
+
+    // Add includes
+    if (this._includes.length > 0) {
+        args.push(this._includes.map(function(pattern) {
+            return buildOption('include', pattern);
+        }).join(' '));
+    }
+
+    // Add source(s) and destination
+    args.push(
+        this.source().join(' '),
+        this.destination()
+    );
+
+    return args;
+};
+
+/**
+ * Execute the rsync command.
+ * @param {Function} callback
+ */
+Rsync.prototype.execute = function(callback) {
+    // Execute the command in a subshell
+    var cmd  = this.command();
+
+    // output buffers
+    var stdoutBuffer = '',
+        stderrBuffer = Buffer('');
+
+    // Execute the command and wait for it to finish
+    var command = exec(cmd);
+
+    // capture stdout and stderr
+    command.stdout.on('data', function(chunk) { stdoutBuffer += chunk; });
+    command.stderr.on('data', function(chunk) { stderrBuffer += chunk; });
+
+    command.on('exit', function(code) {
+        var error = null;
+
+        // Check rsyncs error code
+        // @see http://bluebones.net/2007/06/rsync-exit-codes/
+        if (code !== 0) {
+            error = new Error('rsync exited with code ' + code);
+        }
+
+        // Check for callback
+        if (typeof(callback) === 'function') {
+            callback(error, stdoutBuffer, stderrBuffer, cmd);
+        }
+    });
+};
+
+createValueAccessor('debug');
+createValueAccessor('executable');
+createValueAccessor('destination');
+
+createListAccessor('source', '_sources');
+createListAccessor('exclude', '_excludes');
+createListAccessor('include', '_includes');
+
+exposeLongOption('rsh', 'shell');
+
+exposeShortOption('progress');
+exposeShortOption('a', 'archive');
+exposeShortOption('z', 'compress');
+exposeShortOption('r', 'recursive');
+exposeShortOption('u', 'update');
+exposeShortOption('q', 'quiet');
+exposeShortOption('d', 'dirs');
+exposeShortOption('l', 'links');
+exposeShortOption('n', 'dry');
+
+// our awesome export products
+module.exports = Rsync;
+
+/* **** */
+
+/**
+ * Create a chainable function on the Rsync prototype for getting and setting an
+ * internal value.
+ * @param {String} name
+ * @param {String} internal
+ */
+function createValueAccessor(name, internal) {
+    var container = internal || '_' + name;
+
+    Rsync.prototype[name] = function(value) {
+        if (!arguments.length) return this[container];
+        this[container] = value;
+        return this;
+    };
+}
+
+/**
+ * @param {String} name
+ * @param {String} internal
+ */
+function createListAccessor(name, internal) {
+    var container = internal || '_' + name;
+
+    Rsync.prototype[name] = function(value) {
+        if (!arguments.length) return this[container];
+
+        if (isArray(value)) {
+            value.forEach(this[name], this);
+        }
+        else if (typeof(value) !== 'string') {
+            throw new Error('Value for Rsync::' + name + ' must be a String');
+        }
+        else if (this[container].indexOf(value) < 0) {
+            this[container].push(value);
+        }
+
+        return this;
+    };
+}
+
+/**
+ * Create a shorthand method on the Rsync prototype for setting and unsetting a simple option.
+ * @param {String} option
+ * @param {String} name
+ */
+function exposeShortOption(option, name) {
+    name = name || option;
+
+    Rsync.prototype[name] = function(set) {
+        // When no arguments are passed in assume the option
+        // needs to be set
+        if (!arguments.length) set = true;
+
+        var method = (set) ? 'set' : 'unset';
+        return this[method](option);
+    };
+}
+
+/**
+ * Expose an rsync long option on the Rsync prototype.
+ * @param {String} option   The option to expose
+ * @param {String} name     An optional alternative name for the option.
+ */
+function exposeLongOption(option, name) {
+    name = name || option;
+
+    Rsync.prototype[name] = function(value) {
+        // When not arguments are passed in assume the options
+        // current value is requested
+        if (!arguments.length) return this.option(option);
+
+        var method = (value) ? 'set' : 'unset';
+        return this[method](option, value);
+    };
+}
+
+/**
+ * Build an option for use in a shell command.
+ * @param {String} name
+ * @param {String} vlaue
+ * @return {String}
+ */
+function buildOption(name, value) {
+    var single = (name.length === 1) ? true : false;
+
+    // Decide on prefix and value glue
+    var prefix = (single) ? '-' : '--';
+    var glue   = (single) ? ' ' : '=';
+
+    // Build the option
+    var option = prefix + name;
+    if (arguments.length > 1 && value) {
+        option += glue + escapeShellArg(String(value));
+    }
+
+    return option;
+}
+
+/**
+ * Escape an argument for use in a shell command.
+ * @param {String} arg
+ * @return {String}
+ */
+function escapeShellArg(arg) {
+  return '"' + arg.replace(/(["\s'$`\\])/g, '\\$1') + '"';
+}
+
+/**
+ * Strip the leading dashes from a value.
+ * @param {String} value
+ * @return {String}
+ */
+function stripLeadingDashes(value) {
+    if (typeof(value) === 'string') {
+        value = value.replace(/^[\-]*/, '');
+    }
+
+    return value;
+}
+
+/**
+ * Simple method for capturing output in a String
+ * @param {ReadableStream} stream
+ * @param {String} buffer
+ */
+function captureOutput(stream, buffer) {
+    stream.on('data', function(chunk) {
+        buffer += chunk;
+    });
+}
+
+/**
+ * Simple function for checking if a value is an Array. Will use the native
+ * Array.isArray method if available.
+ * @param {Mixed} value
+ * @return {Boolean}
+ */
+function isArray(value) {
+    if (typeof(Array.isArray) === 'function') {
+        return Array.isArray(value);
+    }
+    else {
+        return toString.call(value) == '[object Array]';
+    }
+}
+
+/**
+ * Simple hasOwnProperty wrapper. This will call hasOwnProperty on the obj
+ * through the Object prototype.
+ * @param {Object} obj  The object to check the property on
+ * @param {String} key  The name of the property to check
+ * @return {Boolean}
+ */
+function hasOP(obj, key) {
+    return Object.prototype.hasOwnProperty.call(obj, key);
+}
